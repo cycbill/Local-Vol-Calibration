6f6b867fe41d5c52eaf5822f0336203822a3e720
cycbill <cuiyechang1991@gmail.com>
2019-11-27 16:55:58 -0500

    Rannacher scheme on PDE

--- source/fwd_fdm_main.py
+++ source/fwd_fdm_main.py
@@ -7,7 +7,7 @@ from parameters import PiecewiseLinearParameter1D
 from rate_curve_class import RateCurve
 from tenor_market_data import TenorMarketData
 from initial_condition import InitialConditionFirstTenor
-from fwd_fdm import FDMCrankNicolsonNeumann
+from fwd_fdm_2 import FDMCrankNicolsonNeumann
 from black_scholes_formulas import *
 
 ## Trade and single data
@@ -21,9 +21,10 @@ r_inputs = np.array([0.05, 0.05])
 rf_inputs = np.array([0.0, 0.0])
 r_para = RateCurve(T_inputs, r_inputs)
 rf_para = RateCurve(T_inputs, rf_inputs)
+csc_para = RateCurve(T_inputs, rf_inputs)
 
 ## Tenor Market Data
-tenor_mkt_data = TenorMarketData(S, r_para, rf_para, T)
+tenor_mkt_data = TenorMarketData(S, r_para, rf_para, csc_para, T)
 
 ## k inputs
 imp_vol_atm = 0.2
@@ -31,17 +32,23 @@ loc_vol_inputs = np.array([0.25, 0.15])
 k_inputs = np.array([-5*imp_vol_atm*np.sqrt(T), 5*imp_vol_atm*np.sqrt(T)])
 K_inputs = tenor_mkt_data.fwd * np.exp(k_inputs)
 
+J = 200
 x_min = k_inputs[0]
 x_max = k_inputs[-1]
-J = 200
+x_values = np.linspace(x_min, x_max, J+1, endpoint=True)
+N = 2000
 t_min = 0
 t_max = T
-N = 2000
+t_values = np.linspace(t_min, t_max, N+1, endpoint=True)
+theta = np.repeat(0.5, N)
+theta[0] = 0
+theta[1:5] = [1.0, 1.0, 1.0, 1.0]
+
 
 loc_vol_para = PiecewiseLinearParameter1D(k_inputs, loc_vol_inputs)
 
 init_cond = InitialConditionFirstTenor()
-fdm_cn = FDMCrankNicolsonNeumann(x_min, x_max, J, t_min, t_max, N, tenor_mkt_data, loc_vol_para, init_cond)
+fdm_cn = FDMCrankNicolsonNeumann(x_min, x_max, x_values, J, t_min, t_max, t_values, theta, N, tenor_mkt_data, loc_vol_para, init_cond)
 
 prices, x_values = fdm_cn.step_march()
 plt.show()
@@ -63,7 +70,7 @@ premium_bs_implied_vol = black_scholes_vanilla(callput, S, K_outputs[1:-1], T, t
 
 
 ## Output pde results
-wb = xw.Book('LocVol Parameters.xlsx')
+wb = xw.Book(r'source/LocVol Parameters.xlsx')
 sht = wb.sheets['FDM_Output']
 
 sht.range('E4').options(transpose=True).value = x_values
