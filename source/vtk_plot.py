986c958b00faa4a35d345f602a3eabfbdbbd6396
cycbill <cuiyechang1991@gmail.com>
2019-12-08 23:28:43 -0500

    plot vtk

--- source/vtk_plot.py
+++ source/vtk_plot.py
@@ -4,8 +4,10 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
-folder = 'D:\OneDrive - Murex\Desktop\Local Vol Macs Traces\Single Continuous Partial Barrier'
-file = 'pde1d_mx21483vm_11652_9.vtk'
+from vtk_visualization import vtk_visualization
+
+folder = r'C:\Users\van92\OneDrive\Desktop\Local-Vol-Calibration\data\Local Vol Macs Traces\Vanilla TARF'
+file = 'pde1d_mx21483vm_11652_22.vtk'
 
 vtk_data = pd.read_csv(folder + '\\' + file)
 vtk_data.columns = ['Data']
@@ -15,7 +17,14 @@ data = vtk_data.iloc[:,0].str.find('POINTS')
 
 
 a_idx = vtk_data.index[vtk_data['Data'].str.contains('DATASET STRUCTURED_GRID')].tolist()[0]
-b_idx = vtk_data.index[vtk_data['Data'].str.contains('SCALARS Op_Q0_Npv double')].tolist()[0]
+
+b_idx_list = vtk_data.index[vtk_data['Data'].str.contains('SCALARS Op_Q0_Npv double')].tolist()
+if b_idx_list != []:
+    b_idx = b_idx_list[0]
+else:
+    b_idx = vtk_data.index[vtk_data['Data'].str.contains('SCALARS Npv_Q0_Npv double')].tolist()[0]
+
+
 c_idx = vtk_data.index[vtk_data['Data'].str.contains('SCALARS Spot double')].tolist()[0]
 
 grid_start_idx = a_idx + 3
@@ -34,7 +43,6 @@ grid_table5 = grid_table4.astype(float)
 t_grid = grid_table5[:, 1]
 k_index = [i for i in range(0, grid_table5.shape[1], 3)]
 k_grid = grid_table5[0, k_index]
-print(k_grid)
 
 
 value_table = vtk_data.loc[value_start_idx:value_end_idx, :]
@@ -42,33 +50,10 @@ value_table2 = value_table['Data'].str.split(' ', expand=True)
 value_table3 = value_table2.values
 value_table4 = value_table3[:,:-1]
 value_table5 = value_table4.astype(float)
-print(value_table5)
-prem = value_table5
-print(prem.shape, len(t_grid), len(k_grid))
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
+
 k, t = np.meshgrid(k_grid, t_grid)
-ax.plot_surface(k, t, prem, cmap=cm.coolwarm)
-plt.show()
+prem = value_table5
 
 
-'''
-for row in grid_table3:
-    print(type(row))
-    grid_table5 = np.fromstring(row, sep=' ')
-    print(grid_table5)
-
-#grid_table = grid_table['Data'].str.rstrip()
-df_value = vtk_data.loc[value_start_idx:value_end_idx, :]
-#df_value = df_value['Data'].str.rstrip()
-
-grid_table2 = grid_table['Data'].str.split(' ', expand=True) #
-
-t_grid = grid_table2[1]
-
-
-np_grid_string = grid_table2.values
-#np_grid = np_grid_string.astype(np.float)
-print(np_grid_string)
-'''
+vtk_visualization(k_grid, t_grid, k, t, prem)
--- source/vtk_visualization.py
+++ source/vtk_visualization.py
@@ -4,53 +4,60 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
-wb = xw.Book('LocVol Parameters.xlsx')
-sht = wb.sheets['VTK']
+def vtk_visualization(k_grids, t_grids, k, t, v_values):
 
-t_grids = sht.range('A2').options(np.array, expand='down').value
-k_grids = sht.range('B1').options(np.array, expand='right').value
-v_values = sht.range('B2').options(np.array, expand='table').value
+    ## VTK plot
+    fig = plt.figure()
+    ax = fig.gca(projection = '3d')
+    ax.plot_surface(k, t, v_values, cmap=cm.coolwarm)
+    ax.set_xlabel('k')
+    ax.set_ylabel('t')
+    ax.set_zlabel('v')
+    ax.set_title('VTK')
+    plt.show()
 
-print(v_values.shape)
+    ## T steps plot
+    fig = plt.figure(figsize=plt.figaspect(0.5))
+    fig.add_subplot(121)
+    ax1 = fig.add_subplot(121)
+    ax1.plot(t_grids, '.-', linewidth=1.0, ms=4.0)
+    ax1.grid(True, which='both')
 
-k, t = np.meshgrid(k_grids, t_grids)
+    ax2 = fig.add_subplot(122)
+    ax2.plot(-1.0 * np.diff(t_grids), '.-', linewidth=1.0, ms=4.0)
+    ax2.grid(True, which='both')
+    ax2.set_ylim(bottom = 0.0)
 
-## VTK plot
-fig = plt.figure()
-ax = fig.gca(projection = '3d')
-ax.plot_surface(k, t, v_values, cmap=cm.coolwarm)
-ax.set_xlabel('k')
-ax.set_ylabel('t')
-ax.set_zlabel('v')
-ax.set_title('VTK')
-plt.show()
+    plt.suptitle('T steps')
+    plt.show()
 
-## T steps plot
-fig = plt.figure(figsize=plt.figaspect(0.5))
-fig.add_subplot(121)
-ax1 = fig.add_subplot(121)
-ax1.plot(t_grids, '.-', linewidth=1.0, ms=4.0)
-ax1.grid(True, which='both')
+    ## k steps plot
+    fig = plt.figure(figsize=plt.figaspect(0.5))
+    fig.add_subplot(121)
+    ax1 = fig.add_subplot(121)
+    ax1.plot(k_grids, '.-', linewidth=1.0, ms=4.0)
+    ax1.grid(True, which='both')
 
-ax2 = fig.add_subplot(122)
-ax2.plot(-1.0 * np.diff(t_grids), '.-', linewidth=1.0, ms=4.0)
-ax2.grid(True, which='both')
-ax2.set_ylim(bottom = 0.0)
+    ax2 = fig.add_subplot(122)
+    ax2.plot(np.diff(k_grids), '.-', linewidth=1.0, ms=4.0)
+    ax2.grid(True, which='both')
+    ax2.set_ylim(bottom = 0.0)
 
-plt.suptitle('T steps')
-plt.show()
+    plt.suptitle('k steps')
+    plt.show()
 
-## k steps plot
-fig = plt.figure(figsize=plt.figaspect(0.5))
-fig.add_subplot(121)
-ax1 = fig.add_subplot(121)
-ax1.plot(k_grids, '.-', linewidth=1.0, ms=4.0)
-ax1.grid(True, which='both')
 
-ax2 = fig.add_subplot(122)
-ax2.plot(np.diff(k_grids), '.-', linewidth=1.0, ms=4.0)
-ax2.grid(True, which='both')
-ax2.set_ylim(bottom = 0.0)
+if __name__ == '__main__':
 
-plt.suptitle('k steps')
-plt.show()
+    wb = xw.Book('LocVol Parameters.xlsx')
+    sht = wb.sheets['VTK']
+
+    t_grids = sht.range('A2').options(np.array, expand='down').value
+    k_grids = sht.range('B1').options(np.array, expand='right').value
+    v_values = sht.range('B2').options(np.array, expand='table').value
+
+    print(v_values.shape)
+
+    k, t = np.meshgrid(k_grids, t_grids)
+
+    vtk_visualization(k_grids, t_grids, k, t, v_values)
